2afb20daa0a6e9e26d7ff38a81740cda2a82b74d
Lokijopu <kguo2134@gmail.com>
2018-09-12 13:04:46 -0400

    Add files via upload

---
+++ smiley_face/Smiley_face.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Kevin Guo 
+ *Smiley_face
+ */
+public class Smiley_face {
+
+	public static void main(String[] args) {
+		System.out.println("\\\\\\\\\\\\");
+		System.out.println("| * * |");
+		System.out.println("|  *  |");
+		System.out.println("| \\_/ \\");
+		System.out.println("\\_____/");
+
+	}
+
+}
